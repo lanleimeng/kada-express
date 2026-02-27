938c600d944ef6ce12af70838a74a1ab7eb5be7f
lanleimeng <133726463+lanleimeng@users.noreply.github.com>
2026-02-27 10:35:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,9 +16,14 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/api/notes", noteRoutes);
+app.use("/", noteRoutes);
 
 
 app.use(errorHandler);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
 
 export default app; 
