7b27e6b3840d2cfd84f7a2871d8e8189944ac7e6
lanleimeng <133726463+lanleimeng@users.noreply.github.com>
2026-02-27 10:24:21 +0700

    test
    test

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
-import connectDB from "../config/db.js";
-import noteRoutes from "../routes/note.routes.js";
-import errorHandler from "../middlewares/error.middleware.js";
+import connectDB from "./config/db.js";
+import noteRoutes from "./routes/note.routes.js";
+import errorHandler from "./middlewares/error.middleware.js";
 app.use(cors());
 
 
