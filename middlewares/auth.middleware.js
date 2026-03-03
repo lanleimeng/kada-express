28efd22554efb1d606fbd85fb79cf6b42606018c
lanleimeng <133726463+lanleimeng@users.noreply.github.com>
2026-03-03 13:54:55 +0700

    update
    update

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -10,6 +10,7 @@ export const authenticateToken = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded; 
+    console.log("Decoded token:", decoded);
     next();
   } catch (err) {
     return res.status(403).json({ message: "Invalid token" });
